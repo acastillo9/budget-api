978ebb5d71eac82ddad5ba9bc18f445e8c5cc7de
Andrés Castillo <info@acastillo.dev>
2025-07-18 09:09:35 -0500

    Add delete transaction and transfer

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -12,7 +12,6 @@ import {
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { TransactionsQueryDto } from './dto/transactions-query.dto';
 import { AuthenticatedRequest } from 'src/shared/types';
 import { CreateTransferDto } from './dto/create-transfer.dto';
 import { PaginationDto } from 'src/shared/dto/pagination.dto';
@@ -117,62 +116,28 @@ export class TransactionsController {
     );
   }
 
-  // /**
-  //  * Find all transactions of an user. Optionally filter by account, month and year.
-  //  * @param req The request object.
-  //  * @param query The query parameters to filter the transactions.
-  //  * @returns The transactions found.
-  //  * @async
-  //  */
-  // @Get()
-  // findAllByAccount(
-  //   @Request() req: AuthenticatedRequest,
-  //   @Query() query: TransactionsQueryDto,
-  // ) {
-  //   return this.transactionsService.findAll(query, req.user.id);
-  // }
-  //
-  // /**
-  //  * Find a transaction by id.
-  //  * @param req The request object.
-  //  * @param id The id of the transaction to find.
-  //  * @returns The transaction found.
-  //  * @async
-  //  */
-  // @Get(':id')
-  // findOne(@Request() req: AuthenticatedRequest, @Param('id') id: string) {
-  //   return this.transactionsService.findOne(id, req.user.id);
-  // }
-  //
-  // /**
-  //  * Update a transaction by id.
-  //  * @param req The request object.
-  //  * @param id The id of the transaction to update.
-  //  * @param updateTransactionDto The data to update the transaction.
-  //  * @returns The transaction updated.
-  //  * @async
-  //  */
-  // @Patch(':id')
-  // update(
-  //   @Request() req: AuthenticatedRequest,
-  //   @Param('id') id: string,
-  //   @Body() updateTransactionDto: UpdateTransactionDto,
-  // ) {
-  //   return this.transactionsService.update(
-  //     id,
-  //     updateTransactionDto,
-  //     req.user.id,
-  //   );
-  // }
-  //
-  // /**
-  //  * Remove a transaction by id.
-  //  * @param req The request object.
-  //  * @param id The id of the transaction to remove.
-  //  * @async
-  //  */
-  // @Delete(':id')
-  // remove(@Request() req: AuthenticatedRequest, @Param('id') id: string) {
-  //   return this.transactionsService.remove(id, req.user.id);
-  // }
+  /**
+   * Remove a transaction by id.
+   * @param req The request object.
+   * @param id The id of the transaction to remove.
+   * @async
+   */
+  @Delete(':id')
+  remove(@Request() req: AuthenticatedRequest, @Param('id') id: string) {
+    return this.transactionsService.remove(id, req.user.userId);
+  }
+
+  /**
+   * Remove a transfer transaction by id.
+   * @param req The request object.
+   * @param id The id of the transfer transaction to remove.
+   * @async
+   */
+  @Delete('transfer/:id')
+  removeTransfer(
+    @Request() req: AuthenticatedRequest,
+    @Param('id') id: string,
+  ) {
+    return this.transactionsService.removeTransfer(id, req.user.userId);
+  }
 }
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,17 +1,15 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { Transaction } from './entities/transaction.entity';
-import { Connection, Model } from 'mongoose';
+import { Model } from 'mongoose';
 import { AccountsService } from 'src/accounts/accounts.service';
 import { TransactionDto } from './dto/transaction.dto';
 import { plainToClass } from 'class-transformer';
-import { TransactionsQueryDto } from './dto/transactions-query.dto';
 import { DbTransactionService } from 'src/shared/db-transaction.service';
 import { CategoriesService } from 'src/categories/categories.service';
 import { CategoryType } from 'src/categories/entities/category-type.enum';
-import { CategoryDto } from 'src/categories/dto/category.dto';
 import { I18nService } from 'nestjs-i18n';
 import { CreateTransferDto } from './dto/create-transfer.dto';
 import { PaginationDto } from 'src/shared/dto/pagination.dto';
@@ -328,16 +326,6 @@ export class TransactionsService {
     updateTransferDto: UpdateTransferDto,
     userId: string,
   ): Promise<TransactionDto> {
-    // {
-    //   id: '687423041e7fb2a5a7db79e6',
-    //   amount: 2000000,
-    //   date: '2025-07-13',
-    //   description: 'pago TC',
-    //   notes: undefined,
-    //   originAccount: '685ed4eb9a2a9117ab863058',
-    //   account: '685ed4f89a2a9117ab86305f'
-    // }
-
     const transactionToUpdate = await this.findOne(id, userId);
     if (!transactionToUpdate.isTransfer) {
       throw new HttpException(
@@ -536,6 +524,80 @@ export class TransactionsService {
   }
 
   /**
+   * Remove a transaction by id.
+   * @param id The id of the transaction to remove.
+   * @param userId The id of the user to remove the transaction.
+   * @returns The transaction removed.
+   * @async
+   */
+  async remove(id: string, userId: string): Promise<TransactionDto> {
+    const transaction = await this.findOne(id, userId);
+    return this.dbTransactionService.runTransaction(async (session) => {
+      // Remove the transaction amount from the account balance
+      await this.accountsService.addAccountBalance(
+        transaction.account.id,
+        -transaction.amount,
+        session,
+      );
+      // Delete the transaction
+      const deletedTransaction = await this.transactionModel.findOneAndDelete(
+        { _id: id, user: userId },
+        { session },
+      );
+      if (!deletedTransaction) {
+        throw new HttpException('Transaction not found', HttpStatus.NOT_FOUND);
+      }
+      return plainToClass(TransactionDto, deletedTransaction.toObject());
+    });
+  }
+
+  /**
+   * Remove a transfer transaction by id.
+   * @param id The id of the transfer transaction to remove.
+   * @param userId The id of the user to remove the transfer transaction.
+   * @returns The transfer transaction removed.
+   * @async
+   */
+  async removeTransfer(id: string, userId: string): Promise<TransactionDto> {
+    const transaction = await this.findOne(id, userId);
+    if (!transaction.isTransfer) {
+      throw new HttpException(
+        'Transaction is not a transfer',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.dbTransactionService.runTransaction(async (session) => {
+      // Remove the transfer amount from both accounts involved in the transfer
+      await this.accountsService.addAccountBalance(
+        transaction.account.id,
+        -transaction.amount,
+        session,
+      );
+      await this.accountsService.addAccountBalance(
+        transaction.transfer.account.id,
+        -transaction.transfer.amount,
+        session,
+      );
+      // Delete both transactions
+      const deletedTransaction = await this.transactionModel.findOneAndDelete(
+        { _id: id, user: userId },
+        { session },
+      );
+      const deletedTransfer = await this.transactionModel.findOneAndDelete(
+        { _id: transaction.transfer.id, user: userId },
+        { session },
+      );
+      if (!deletedTransaction || !deletedTransfer) {
+        throw new HttpException(
+          'Transfer transaction not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return plainToClass(TransactionDto, deletedTransaction.toObject());
+    });
+  }
+
+  /**
    * Find a transaction by id.
    * @param id The id of the transaction to find.
    * @param userId The id of the user to find the transaction.
@@ -563,193 +625,4 @@ export class TransactionsService {
       );
     }
   }
-
-  //
-  // /**
-  //  * Find all transactions of an user. The filter can be by account, month and year.
-  //  * @param query The query parameters to filter the transactions.
-  //  * @param userId The id of the user to find the transactions.
-  //  * @returns The transactions found.
-  //  * @async
-  //  */
-  // async findAll(query: TransactionsQueryDto, userId: string) {
-  //   if (!query.month || !query.year) {
-  //     throw new HttpException(
-  //       'Invalid query parameters',
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
-  //
-  //   const dbQuery: Record<string, any> = { user: userId };
-  //   if (query.accountId) {
-  //     dbQuery['account'] = query.accountId;
-  //   }
-  //
-  //   if (query.month && query.year) {
-  //     const startOfMonth = new Date(query.year, query.month - 1, 1);
-  //     const endOfMonth = new Date(query.year, query.month, 1);
-  //     dbQuery['startDate'] = { $gte: startOfMonth, $lt: endOfMonth };
-  //   }
-  //
-  //   try {
-  //     const transactions = await this.transactionModel.find(dbQuery);
-  //     return transactions.map((transaction) =>
-  //       plainToClass(TransactionDto, transaction.toObject()),
-  //     );
-  //   } catch (error) {
-  //     this.logger.error(
-  //       `Failed to find transactions for user ${userId} with query ${JSON.stringify(query)}: ${error.message}`,
-  //       error.stack,
-  //     );
-  //     throw new HttpException(
-  //       'Error finding the transactions',
-  //       HttpStatus.INTERNAL_SERVER_ERROR,
-  //     );
-  //   }
-  // }
-  //
-  // /**
-  //  * Find a transaction by id.
-  //  * @param id The id of the transaction to find.
-  //  * @param userId The id of the user to find the transaction.
-  //  * @returns The transaction found.
-  //  * @async
-  //  */
-  // async findOne(id: string, userId: string): Promise<TransactionDto> {
-  //   try {
-  //     const transaction = await this.transactionModel.findOne({
-  //       _id: id,
-  //       user: userId,
-  //     });
-  //     if (!transaction) {
-  //       throw new HttpException('Transaction not found', HttpStatus.NOT_FOUND);
-  //     }
-  //     return plainToClass(TransactionDto, transaction.toObject());
-  //   } catch (error) {
-  //     this.logger.error(
-  //       `Failed to find transaction: ${error.message}`,
-  //       error.stack,
-  //     );
-  //     throw new HttpException(
-  //       'Error finding the transaction',
-  //       HttpStatus.INTERNAL_SERVER_ERROR,
-  //     );
-  //   }
-  // }
-  //
-  // /**
-  //  * Update a transaction by id.
-  //  * @param id The id of the transaction to update.
-  //  * @param updateTransactionDto The data to update the transaction.
-  //  * @param userId The id of the user to update the transaction.
-  //  * @returns The transaction updated.
-  //  * @async
-  //  */
-  // async update(
-  //   id: string,
-  //   updateTransactionDto: UpdateTransactionDto,
-  //   userId: string,
-  // ): Promise<TransactionDto> {
-  //   const oldTransaction = await this.findOne(id, userId);
-  //
-  //   const session = await this.connection.startSession();
-  //   session.startTransaction();
-  //   try {
-  //     // update the transaction
-  //     const updatedTransaction = await this.transactionModel.findOneAndUpdate(
-  //       { _id: id, user: userId },
-  //       {
-  //         ...updateTransactionDto,
-  //         amount:
-  //           updateTransactionDto.transactionType === TransactionType.EXPENSE
-  //             ? -updateTransactionDto.amount
-  //             : updateTransactionDto.amount,
-  //       },
-  //       { new: true },
-  //     );
-  //
-  //     // fix the account balance if the transaction was paid or the amount was changed
-  //     if (updatedTransaction.paid != oldTransaction.paid) {
-  //       const account = await this.accountsService.addAccountBalance(
-  //         updatedTransaction.account.id,
-  //         updatedTransaction.paid
-  //           ? updatedTransaction.amount
-  //           : -oldTransaction.amount,
-  //       );
-  //       updatedTransaction.account.balance = account.balance;
-  //     } else if (
-  //       updatedTransaction.paid &&
-  //       updatedTransaction.amount !== oldTransaction.amount
-  //     ) {
-  //       const amountDiff = updatedTransaction.amount - oldTransaction.amount;
-  //       const account = await this.accountsService.addAccountBalance(
-  //         updatedTransaction.account.id,
-  //         amountDiff,
-  //       );
-  //       updatedTransaction.account.balance = account.balance;
-  //     }
-  //
-  //     await session.commitTransaction();
-  //     return plainToClass(TransactionDto, updatedTransaction.toObject());
-  //   } catch (error) {
-  //     await session.abortTransaction();
-  //     this.logger.error(
-  //       `Failed to update transaction: ${error.message}`,
-  //       error.stack,
-  //     );
-  //     throw new HttpException(
-  //       'Error updating the transaction',
-  //       HttpStatus.INTERNAL_SERVER_ERROR,
-  //     );
-  //   } finally {
-  //     await session.endSession();
-  //   }
-  // }
-  //
-  // /**
-  //  * Remove a transaction by id.
-  //  * @param id The id of the transaction to remove.
-  //  * @param userId The id of the user to remove the transaction.
-  //  * @returns The transaction removed.
-  //  * @async
-  //  */
-  // async remove(id: string, userId: string): Promise<TransactionDto> {
-  //   const session = await this.connection.startSession();
-  //   session.startTransaction();
-  //   try {
-  //     const transaction = await this.transactionModel.findOneAndDelete({
-  //       _id: id,
-  //       user: userId,
-  //     });
-  //     if (!transaction) {
-  //       throw new HttpException('Transaction not found', HttpStatus.NOT_FOUND);
-  //     }
-  //
-  //     // if is a paid transaction, remove the amount from the account balance
-  //     if (transaction.paid) {
-  //       const account = await this.accountsService.addAccountBalance(
-  //         transaction.account.id,
-  //         -transaction.amount,
-  //       );
-  //       transaction.account.balance = account.balance;
-  //     }
-  //     await session.commitTransaction();
-  //     return plainToClass(TransactionDto, transaction.toObject());
-  //   } catch (error) {
-  //     await session.abortTransaction();
-  //     if (error instanceof HttpException) {
-  //       throw error;
-  //     }
-  //     this.logger.error(
-  //       `Failed to remove transaction: ${error.message}`,
-  //       error.stack,
-  //     );
-  //     throw new HttpException(
-  //       'Error removing the transaction',
-  //       HttpStatus.INTERNAL_SERVER_ERROR,
-  //     );
-  //   } finally {
-  //     await session.endSession();
-  //   }
-  // }
 }
